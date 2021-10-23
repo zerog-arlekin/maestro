a20c205389b71839f0d38e7c711f633220c637ac
kryvonos <kryvonos.an@gmail.com>
2021-10-23 17:26:14 +0300

    add angular simple

---
+++ src/app/app.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
+
+import { ZoomMtg } from '@zoomus/websdk';
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
+// loads language files, also passes any error messages to the ui
+ZoomMtg.i18n.load('en-US');
+ZoomMtg.i18n.reload('en-US');
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
+  signatureEndpoint = 'http://localhost:4000';
+  apiKey = 'iuxd0UZ4QcSXaAfRS7fnNA';
+  meetingNumber = '84792430728';
+  role = 0;
+  leaveUrl = 'http://localhost:4200';
+  userName = 'Angular';
+  userEmail = '';
+  passWord = 'RUVZYUEwcFJBazNmckxrblhBRDFKZz09';
+  // pass in the registrant's token if your meeting or webinar requires registration. More info here:
+  // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/build/meetings/join#join-registered
+  // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/build/webinars/join#join-registered-webinar
+  registrantToken = '';
+
+  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document) {}
+
+  ngOnInit() {
+
+  }
+
+  getSignature() {
+    this.httpClient.post(this.signatureEndpoint, {
+	    meetingNumber: this.meetingNumber,
+	    role: this.role
+    }).toPromise().then((data: any) => {
+      if (data.signature) {
+        console.log(data.signature);
+        this.startMeeting(data.signature);
+      } else {
+        console.log(data);
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  startMeeting(signature) {
+
+    document.getElementById('zmmtg-root').style.display = 'block';
+
+    ZoomMtg.init({
+      leaveUrl: this.leaveUrl,
+      success: (success) => {
+        console.log(success);
+        ZoomMtg.join({
+          signature,
+          meetingNumber: this.meetingNumber,
+          userName: this.userName,
+          apiKey: this.apiKey,
+          userEmail: this.userEmail,
+          passWord: this.passWord,
+          tk: this.registrantToken,
+          success: (success) => {
+            console.log(success);
+          },
+          error: (error) => {
+            console.log(error);
+          }
+        });
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+}
