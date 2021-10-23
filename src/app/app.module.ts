ea68bfa88104d6b548b3035285feab8d7b9fc219
kryvonos <kryvonos.an@gmail.com>
2021-10-23 19:00:45 +0300

    update build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app-new.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
