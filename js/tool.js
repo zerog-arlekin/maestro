1a37e153558e4509c6b4cf9260c219f181b63c02
kryvonos <kryvonos.an@gmail.com>
2021-10-25 16:26:04 +0300

    update build

--- js/tool.js
+++ js/tool.js
@@ -35,11 +35,11 @@ var testTool = {
   },
   getMeetingConfig: function () {
     return {
-      mn: '8342915730',
+      mn: '9289684478',
       name: testTool.b64EncodeUnicode(
         document.getElementById("display_name").value
       ),
-      pwd: '',
+      pwd: 'WVdGbTlLYk0vR1FTQTloWmljSU5RQT09',
       role: 0,
       email: '',
       lang: 'en-US',
